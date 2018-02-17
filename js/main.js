661d9727902ee11ab55a6088219e05167c4b46a0
Unknown <marrcel12.marga@gmail.com>
2018-02-17 10:41:33 +0100

    dodanie animacji zmiany koloru przy scrollowaniu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 'use strict';
 
 $(function(){
-    if($(window).scroll(function()>){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>100){
         $("#main-nav").css("background-color","white");
+         $(".nav-link").css("color", "black");   
+        }
+        else {
+            $("#main-nav").css("background-color","");
+            $(".nav-link").css("color", "white");  
+        }
     });
 }
 );
